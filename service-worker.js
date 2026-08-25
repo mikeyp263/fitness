47e0f95eb7cdc2e652580edb355a598b78055559
Michael Patterson <90581648+mikeyp263@users.noreply.github.com>
2026-08-24 22:41:50 -0400

    Refresh R14 offline cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='r14-v9-progression-completion';
+const CACHE='r14-v10-evidence-cardio-order';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
