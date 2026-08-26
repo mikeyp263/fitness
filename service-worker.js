d8018106fd56a53eeda7ab8be23645ed7b413dbf
Michael Patterson <90581648+mikeyp263@users.noreply.github.com>
2026-08-26 09:17:26 -0400

    Refresh cache for flexible R14 plan

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='r14-v10-evidence-cardio-order';
+const CACHE='r14-v11-flexible-food-run-walk';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
