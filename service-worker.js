8723e115d22b9155ffa54cf18ea3a1f54661263c
Michael Patterson <90581648+mikeyp263@users.noreply.github.com>
2026-09-18 14:07:24 -0400

    Refresh cache for hybrid athlete lifting split

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='apex-v3-no-medball-20260918';
+const CACHE='apex-v4-hybrid-jacked-20260918';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
