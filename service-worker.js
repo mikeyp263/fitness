57d8b47c576f7348a5ca3b562a58fbea3067b39a
Michael Patterson <90581648+mikeyp263@users.noreply.github.com>
2026-09-22 12:17:21 -0400

    Refresh cache for balanced athletic physique split

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='apex-v5-balanced-hybrid-guides-20260922';
+const CACHE='apex-v6-athletic-physique-20260922';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
