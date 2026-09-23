e75e8075b187de8c32878297965b56f507f101f0
Michael Patterson <90581648+mikeyp263@users.noreply.github.com>
2026-09-23 09:42:05 -0400

    Apply training-week selection immediately and synchronize lifting and Runs views

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'r14-arms-20260923-v7';
+const CACHE = 'r14-weeks-20260923-v8';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 const INDEX = new URL('./index.html', self.location.href).href;
 
